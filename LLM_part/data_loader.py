5b8708045c67062ee51a45a67eb63c50975e7932
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2026-07-03 17:26:12 +0200

    generalise llm prediction

---
+++ LLM_part/data_loader.py
@@ -0,0 +1,76 @@
+# This script computes various metrics for playlist recommendations based on a generated playlist and relevant songs.
+# It loads data from CSV files and a JSON file, processes the data, and calculates metrics such as HIT@N, Precision@N, Recall@N, MRR@N, R-Precision, and NDCG.
+
+###########
+# Imports #
+###########
+
+import csv
+import yaml
+import statistics
+from tqdm import tqdm  # type: ignore
+
+########################
+# Functions definition #
+########################
+
+
+items_csv = "/data/items.csv"
+tracks_csv = "/data/tracks.csv"
+test_set = "/data/cluster_test.csv"
+
+def normalize_song(song):
+    #Normalize a the tuple (track_name, artist_name) by stripping spaces and converting to lowercase.
+    #Returns a tuple (track_name, artist_name).
+
+    return (song[0].strip().lower(), song[1].strip().lower())
+
+
+
+def load_all_playlist_data(items_csv=items_csv, tracks_csv=tracks_csv):
+    #Loads track metadata and the mapping between PID and track once.
+    #Returns a dictionary: {pid: [(track_name, artist_name), ...]}.
+    #This is done to avoid loading the CSV files multiple times.
+    
+    track_metadata = {}
+    with open(tracks_csv, 'r', encoding='utf8') as f:
+        reader = csv.DictReader(f)
+        for row in tqdm(reader, desc="Loading track metadata", unit="track"):
+            track_metadata[row["track_uri"]] = {
+                "track_name": row["track_name"],
+                "artist_name": row["artist_name"],
+            }
+
+    playlist_tracks = {}
+    with open(items_csv, 'r', encoding='utf8') as f:
+        reader = csv.DictReader(f)
+        for row in tqdm(reader, desc="Loading playlist tracks", unit="playlist"):
+            current_pid = row["pid"]
+            track_uri = row["track_uri"]
+            if current_pid not in playlist_tracks:
+                playlist_tracks[current_pid] = []
+            if track_uri in track_metadata:
+                playlist_tracks[current_pid].append(
+                    (track_metadata[track_uri]["track_name"], track_metadata[track_uri]["artist_name"])
+                )
+    return playlist_tracks
+
+def load_playlists_yaml(yaml_path: str) -> dict:
+    """Load playlists from YAML file."""
+    with open(yaml_path, 'r') as f:
+        data = yaml.safe_load(f)
+    return data['playlists']
+
+
+def load_playlists_csv(test_set=test_set) -> list:
+    """Load playlists from CSV file."""
+    playlists = []
+    with open(test_set, 'r', encoding='utf8') as f:
+        reader = csv.DictReader(f)
+        for row in tqdm(reader, desc="Loading playlists from CSV", unit="playlist"):
+            playlist = {
+                "pid": row["Playlist ID"],
+                "playlist_title": row["Playlist Title"],
+            }
+            playlists.append(playlist)
+    return playlists
---
+++ LLM_part/llm_prediction.py
@@ -0,0 +1,218 @@
+# This script generates playlists based on a five-shot prompt using various LLMs.
+# The same code has been adusted to do the zero and one-shot prompts. 
+
+###########
+# Imports #
+###########
+
+import argparse
+import argparse
+from html import parser
+import logging
+import os
+import re
+import yaml # type: ignore
+from openai import OpenAI # type: ignore
+import time
+import torch # type: ignore
+import json
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from langchain_core.prompts.prompt import PromptTemplate # type: ignore
+from data_loader import load_playlists_yaml, load_playlists_csv, load_all_playlist_data
+
+items_csv = "/data/items.csv"
+tracks_csv = "/data/tracks.csv"
+test_set = "/data/cluster_test.csv"
+
+vllm_config = {
+    "base_url": "http://helix01:8000",
+    "max_tokens": 16384,
+    "temperature": 1.0,
+    "top_p": 0.95,
+    "top_k": 64,
+    "enable_thinking": True,
+    "thinking_budget_tokens": 8192
+}
+
+
+# GPU environment and cache configuration
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+os.environ["HUGGINGFACE_HUB_CACHE"] = "/data/huggingface/"
+
+# All LLMs available for use
+# available_llms = {
+#     "zephyr": "TheBloke/zephyr-7B-beta-AWQ",
+#     "dpo": "yunconglong/Truthful_DPO_TomGrc_FusionNet_7Bx2_MoE_13B",
+#     "una": "fblgit/UNA-TheBeagle-7b-v1",
+#     "solar": "bhavinjawade/SOLAR-10B-OrcaDPO-Jawade",
+#     "gpt4": "OpenAI-GPT4",
+#     "llama3.1": "Ollama/llama3.1"  
+# }
+
+available_llms = {
+    "gemma": "google/gemma-4-31B-it",
+    "qwen": "Qwen/Qwen3.6-35B-A3B"
+}
+
+# Set up logging
+loggingFormatString = "%(asctime)s:%(levelname)s:%(threadName)s:%(funcName)s:%(message)s"
+logging.basicConfig(format=loggingFormatString, level=logging.INFO)
+
+########################
+# Functions definition #
+########################
+
+# Load the prompt template from a YAML file
+def load_prompt_template(template_path):
+    with open(template_path, 'r') as file:
+        template_data = yaml.safe_load(file)
+    return PromptTemplate(input_variables=template_data['input'], template=template_data['template'])
+
+## Call the GPT-4 API with retries
+def call_gpt4_api(prompt, model, max_retries=3, timeout=1200):
+    client = OpenAI(
+        base_url=vllm_config["base_url"],
+        api_key=os.getenv("API_KEY"))
+    retries = 0
+    while retries < max_retries:
+        try:
+            response = client.chat.completions.create(
+                model=model,
+                messages=[{"role": "user", "content": prompt}],
+                max_tokens=16_384,
+                temperature=1.0,
+                top_p=0.95,
+                extra_body={
+                    "top_k": 64,
+                    "chat_template_kwargs": {
+                        "enable_thinking": True,
+                    },
+                    "thinking_token_budget": 8_192,
+                },
+            )
+            return str(response.choices[0].message.content).strip()
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            retries += 1
+    return None
+
+# Call the Hugging Face model with GPU support
+def call_huggingface_model(prompt, tokenizer, model):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+    inputs = tokenizer(prompt, return_tensors="pt", max_length=1024, truncation=True).to(device)
+    # Increase max_new_tokens to ensure you get the whole answer
+    outputs = model.generate(**inputs, max_new_tokens=600, eos_token_id=tokenizer.eos_token_id, temperature=0.8)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+    return response
+
+# Generate a playlist using the five-shot prompt
+def generate_playlist_five_shot(seed_playlist, llm_model, template_path, verbose, model_name):
+    PROMPT_TEMPLATE = load_prompt_template(template_path)
+    input_data = {
+        "playlist_title": seed_playlist["playlist_title"],
+        "song1": seed_playlist["song1"],
+        "artist1": seed_playlist["artist1"],
+        "song2": seed_playlist["song2"],
+        "artist2": seed_playlist["artist2"],
+        "song3": seed_playlist["song3"],
+        "artist3": seed_playlist["artist3"],
+        "song4": seed_playlist["song4"],
+        "artist4": seed_playlist["artist4"],
+        "song5": seed_playlist["song5"],
+        "artist5": seed_playlist["artist5"]
+    }
+    formatted_prompt = PROMPT_TEMPLATE.format(**input_data)
+    if verbose:
+        print("Full prompt:", formatted_prompt)
+    res = str(call_gpt4_api(formatted_prompt, llm_model))
+    
+    print("Raw Model Output:", res)
+    try:
+        if "Example:" in res:
+            json_block = re.search(r'Example:\s*(\[[\s\S]+)', res)
+            if json_block:
+                extracted = json_block.group(1).strip()
+            else:
+                print("/!\ No JSON block found after 'Example:'.")
+                return []
+        else:
+            json_block = re.search(r'\[\s*{[\s\S]+?}\s*\]', res)
+            if json_block:
+                extracted = json_block.group()
+            else:
+                print("/!\ No JSON blocks found in the response.")
+                return []
+        if not extracted.endswith(']'):
+            print("Incomplete JSON detected, closing block...")
+            extracted = extracted.rstrip() + "]"
+        generated_playlist = json.loads(extracted)
+        return generated_playlist
+    except json.JSONDecodeError as e:
+        print("JSON parsing error:", e)
+        print("Captured content :", extracted if 'extracted' in locals() else "N/A")
+        return []
+
+
+def main(llm_model, yaml_path=None, template_path='./subset_22.yml', verbose = True):
+    # List of 22 playlists to generate
+    # Each playlist contains a title and 5 songs with their respective artists
+
+    if yaml_path is not None and os.path.exists(yaml_path):  
+        print(f"Loading playlists from {yaml_path}...")
+        playlists = load_playlists_yaml(yaml_path)
+    else:
+        print(f"YAML file {args.yaml} not available. Processing the entire test set.")
+        playlist_tracks = load_all_playlist_data(items_csv, tracks_csv)
+
+        playlists = load_playlists_csv(test_set)
+        for playlist in playlists:
+            playlist['tracks'] = playlist_tracks.get(playlist['pid'], [])[0:5]
+
+    generated_data = {}
+
+    for pl in playlists:
+        print(f"\nGeneration for the playlist '{pl['playlist_title']}' (PID {pl['pid']}) in five-shot...")
+        generated_playlist = generate_playlist_five_shot(
+            pl,
+            llm_model,
+            template_path,
+            verbose,
+            llm_model
+        )
+        generated_data[pl["pid"]] = {
+            "playlist_title": pl["playlist_title"],
+            "song1": pl["song1"],
+            "artist1": pl["artist1"],
+            "song2": pl["song2"],
+            "artist2": pl["artist2"],
+            "song3": pl["song3"],
+            "artist3": pl["artist3"],
+            "song4": pl["song4"],
+            "artist4": pl["artist4"],
+            "song5": pl["song5"],
+            "artist5": pl["artist5"],
+            "generated_playlist": generated_playlist
+        }
+
+    output_dir = "Json_file"
+    os.makedirs(output_dir, exist_ok=True)
+    output_filename = "Five_shot_22_song.json"
+    output_path = os.path.join(output_dir, output_filename)
+
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(generated_data, f, ensure_ascii=False, indent=2)
+
+    print(f"\nThe JSON file was generated in : {output_path}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate playlists using a five-shot prompt with various LLMs.")
+    parser.add_argument("--template_path", type=str, default="prompt_template_five_shot.yml", help="Path to the prompt template YAML file.")
+    parser.add_argument("--llm_model", type=str, default="gemma", choices=available_llms.keys())
+    parser.add_argument(
+            '--yaml', default='./subset_22.yml',
+            help="Path to playlists YAML file (for playlist titles and PIDs)"
+        )
+    args = parser.parse_args()
+    main(args.llm_model, args.yaml, args.template_path)
